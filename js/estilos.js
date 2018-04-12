6099ea404987024ca4e584cdf2787e4cad480d3d
Manupandolfi <numapan@gmail.com>
2018-04-12 15:44:01 -0300

    Fix Definitvo Estilos

--- js/estilos.js
+++ js/estilos.js
@@ -1,19 +1,19 @@
 function cambioEstilo(nro){
 	//Establece un estilo de pagina
 	setObject('estilo',nro);
-	switch(nro){
-		case 0: {$("#css-estilo").attr('href', 'css/default.css');break;}
-		case 1: {$("#css-estilo").attr('href', 'css/estilo1.css');break;}
-		case 2: {$("#css-estilo").attr('href', 'css/estilo2.css');break;}
-	}/*
-	if(nro==1){
-		//setear estilo 1
-		$("#css-estilo").attr('href', 'css/estilo1.css');
+
+	if(nro==0){
+		$("#css-estilo").attr('href', 'css/default.css');
+	}else{
+		if(nro==1){
+			//setear estilo 1
+			$("#css-estilo").attr('href', 'css/estilo1.css');
+		}
+		else{
+			//setear estilo 2
+			$("#css-estilo").attr('href', 'css/estilo2.css');
+		}
 	}
-	else{
-		//setear estilo 2
-		$("#css-estilo").attr('href', 'css/estilo2.css');
-	}*/
 }
 
 function seleccionarEstilo(){
@@ -21,12 +21,10 @@ function seleccionarEstilo(){
 	var estilo = getObject('estilo');
 	//Si es distinto a nulo, es porque seteo algo
 	if(estilo)
-		switch(estilo){
-			case 0: {cambioEstilo(0);break;}
-			case 1: {cambioEstilo(1);break;}
-			case 2: {cambioEstilo(2);break;}
-		}
-	else
-		//Sino, pongo estilo 1 por defecto
+		cambioEstilo(estilo);
+	else{
+		//Sino, pongo estilo 0 por defecto
 		setObject('estilo',0);
+		cambioEstilo(0);
+	}
 }
